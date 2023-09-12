a5b900bef234fcc3b0e5c1b50b0146c95d08ef55
ethanmouri <ethanmouri@gmail.com>
2023-09-12 02:56:45 -0400

    adding dist

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export const formatScore = (highScore) => {
-    //checks if it is undefined or not
-    if(highScore) {
-        //if highscore is defined
-        return (highScore / 1000);
-    }
-    return "(Play to find out)";
-}
-export default formatScore;
